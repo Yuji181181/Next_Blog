ea503a59f1bc4b7745a78351e5edf37dd860991d
Yuji181181 <s1f102402552@iniad.org>
2025-05-12 15:19:56 +0900

    header 修正

--- my-app/src/app/Header.tsx
+++ my-app/src/app/Header.tsx
@@ -7,6 +7,11 @@ const Header = () => {
         <h1 className="text-2xl font-extrabold">
             <Link href="/">Next_Blog</Link>
         </h1>
+        <div>
+          <nav>
+            <Link href="/articles/new">記事を書く</Link>
+          </nav>
+        </div>
     </div>
   )
 }
