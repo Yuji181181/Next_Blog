acc58b34c401854582551c5f5fab125f4f4c7d3a
Yuji181181 <s1f102402552@iniad.org>
2025-05-14 00:45:55 +0900

    list

--- my-app/src/app/components/ArticleList.tsx
+++ my-app/src/app/components/ArticleList.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const ArticleList = () => {
   return (
-    <div>ArticleList</div>
+    <div>
+      <article className="shadow my-4 flex flex-col">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="/blogtest.png"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">Tecnology</Link>
+          <Link href="#" className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4">Next.js_learning</Link>
+          <p className="text-sm pb-3 text-slate-900">pushed on 2025/8/25</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            ああああああああああああああああああああああ
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">続きを読む</Link>
+        </div>
+      </article>
+    </div>
   )
 }
 
