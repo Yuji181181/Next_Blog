f0360c6d6c913345e7b2fa6b851a818dabe596ed
Yuji181181 <s1f102402552@iniad.org>
2025-05-13 13:29:48 +0900

    Revert "修正"
    This reverts commit ded0609c67cdd09c4c792a5e118456004f5d89a4.

--- my-app/src/app/Header.tsx
+++ my-app/src/app/Header.tsx
@@ -3,16 +3,18 @@ import React from 'react'
 
 const Header = () => {
   return (
+  <header className="py-5 px-10">
     <div>
-        <h1 className="text-2xl font-extrabold">
-            <Link href="/">Next_Blog</Link>
-        </h1>
-        <div>
-          <nav>
-            <Link href="/articles/new">記事を書く</Link>
-          </nav>
-        </div>
+      <h1 className="text-2xl font-extrabold">
+        <Link href="/">Next blog</Link>
+      </h1>
     </div>
+    <div>
+      <nav>
+        <Link href="/articles/new">記事を書く</Link>
+      </nav>
+    </div>
+  </header>
   )
 }
 
