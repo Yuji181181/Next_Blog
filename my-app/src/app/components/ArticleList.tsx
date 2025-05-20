11376fcd6e6b7af6e50c09029edec6e8d3563e53
Yuji181181 <s1f102402552@iniad.org>
2025-05-21 00:36:29 +0900

    APIをたたいてmapで展開

--- my-app/src/app/components/ArticleList.tsx
+++ my-app/src/app/components/ArticleList.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Article } from '@/types'
 
-const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+}
+
+const ArticleList = ({articles}: ArticleListProps) => {
   return (
     <div>
-      <article className="shadow my-4 flex flex-col">
-        <Link href="#" className="hover:opacity-75">
+      {articles.map((article) => (
+        <article className="shadow my-4 flex flex-col" key={article.id}>
+        <Link href='articles/${article.id}' className="hover:opacity-75">
           <Image
             src="/blogtest.png"
             alt=""
@@ -15,15 +21,16 @@ const ArticleList = () => {
           />
         </Link>
         <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pb-4 font-bold">Tecnology</Link>
-          <Link href="#" className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4">Next.js_learning</Link>
-          <p className="text-sm pb-3 text-slate-900">pushed on 2025/8/25</p>
-          <Link href="#" className="text-slate-900 pb-6">
-            ああああああああああああああああああああああ
+          <Link href='#' className="text-blue-700 pb-4 font-bold">Technology</Link>
+          <Link href='articles/${article.id}' className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4">{article.title}</Link>
+          <p className="text-sm pb-3 text-slate-900">pushed on {article.createdAt}</p>
+          <Link href='articles/${article.id}' className="text-slate-900 pb-6">
+            {article.content}
           </Link>
-          <Link href="#" className="text-pink-800 hover:text-black">続きを読む</Link>
+          <Link href='articles/${article.id}' className="text-pink-800 hover:text-black">続きを読む</Link>
         </div>
       </article>
+      ))}
     </div>
   )
 }
