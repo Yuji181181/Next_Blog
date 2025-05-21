a4e9d5a52dad2c682e48ba193402d1b17a9bd54c
Yuji181181 <s1f102402552@iniad.org>
2025-05-21 20:07:02 +0900

    fin

--- my-app/src/app/articles/[id]/page.tsx
+++ my-app/src/app/articles/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { getDetailArticle } from '@/blogAPI';
+import DeleteButton from '@/app/components/DeleteButton';
 
 const Article = async ({params}: {params: {id: string}}) => {
   const detailarticle = await getDetailArticle(params.id);
@@ -16,6 +17,9 @@ const Article = async ({params}: {params: {id: string}}) => {
       <h1 className="text-4xl text-center mb-10 mt-10">{detailarticle.title}</h1>
       <div className="text-lg leading-relaxed text-justify">
         <p>{detailarticle.content}</p>
+      </div>
+      <div className="text-right mt-3">
+        <DeleteButton id={detailarticle.id} />
       </div>
     </div>
   )
--- my-app/src/blogAPI.ts
+++ my-app/src/blogAPI.ts
@@ -23,3 +23,48 @@ export const getDetailArticle = async (id: string):Promise<Article> => {
     const article = await res.json();
     return article;
 }
+
+export const createArticle = async (
+    id: string,
+    title: string,
+    content: string,
+):Promise<Article> => {
+    const currentDatetime = new Date().toISOString();
+
+    const res = await fetch('http://localhost:3001/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            id,
+            title,
+            content,
+            createAt: currentDatetime,
+        }),
+    });
+    
+    if (!res.ok) {
+        throw new Error('データの作成に失敗しました');
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const article = await res.json();
+    return article;
+}
+
+export const deleteArticle = async (id: string):Promise<Article> => {
+    const res = await fetch(`http://localhost:3001/posts/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (!res.ok) {
+        throw new Error('データの削除に失敗しました');
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const deletedArticle = await res.json();
+    return deletedArticle;
+}
