a036b289937deea52c81ec7579279108c3f580f4
Yuji181181 <s1f102402552@iniad.org>
2025-05-21 16:56:22 +0900

    詳細のapi

--- my-app/src/blogAPI.ts
+++ my-app/src/blogAPI.ts
@@ -10,3 +10,16 @@ export const getAllArticles = async ():Promise<Article[]> => {
     const articles = await res.json();
     return articles;
 }
+
+export const getDetailArticle = async (id: string):Promise<Article> => {
+    const res = await fetch(`http://localhost:3001/posts/${id}`, {next: {revalidate: 60}});
+
+    if (!res.ok) {
+        throw new Error('データの取得に失敗しました');
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const article = await res.json();
+    return article;
+}
